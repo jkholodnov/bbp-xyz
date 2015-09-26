171f9b1d8470087e8770f6ffba37be6accfe998e
jkholodnov <jkholodnov@gmail.com>
2015-09-26 17:19:18 -0400

    fix api v1

--- projectAPI.py
+++ projectAPI.py
@@ -51,4 +51,4 @@ def get_Team_Analysis_Stats():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True)
